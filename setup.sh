ffbf8164bb3f6a36a6ad18b9bd0f21ff0f5b21e3
nidhoggfgg <1648237021@qq.com>
2019-05-01 16:16:32 +0800

    修复

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd
 pkg in git -y
-mkdir -p .whatandwhy/other
-git clone https://github.com/nidhoggfgg/whatandwhy.git .whatandwhy/whatandwhy
-echo "0" > .whatandwhy/other/depenent.txt
-echo "0" > .whatandwhy/other/people.txt
+mkdir -p $PREFIX/whatandwhy/other
+git clone https://github.com/nidhoggfgg/whatandwhy.git $PREFIX/whatandwhy/whatandwhy
+echo "0" > $PREFIX/whatandwhy/other/depenent.txt
+echo "0" > $PREFIX/whatandwhy/other/people.txt
 echo '#!/bin/bash' > $PREFIX/bin/what
-echo "bash $HOME/.wahtandwhy/whatandwhy/what" >> $PREFIX/bin/what
+echo "bash $PREFIX/wahtandwhy/whatandwhy/what" >> $PREFIX/bin/what
 chmod 777 $PREFIX/bin/what
